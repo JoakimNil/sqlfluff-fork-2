dac08c28c2d7a524e156a09fe37c8731408f0cf2
JoakimNil <24590972+JoakimNil@users.noreply.github.com>
2023-12-11 19:17:35 +0100

    force file save before linting

--- src/main/kotlin/co/anbora/labs/sqlfluff/lint/CustomLinter.kt
+++ src/main/kotlin/co/anbora/labs/sqlfluff/lint/CustomLinter.kt
@@ -1,10 +1,11 @@
 package co.anbora.labs.sqlfluff.lint
 
 import co.anbora.labs.sqlfluff.ide.runner.SqlFluffLintRunner
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.psi.PsiFile
-import java.nio.file.Files
-import kotlin.io.path.extension
+import java.util.concurrent.CompletableFuture
 import kotlin.io.path.pathString
 
 object CustomLinter: Linter() {
@@ -16,6 +17,25 @@ object CustomLinter: Linter() {
         file: PsiFile,
         document: Document
     ): SqlFluffLintRunner.Param {
+        // Create a CompletableFuture
+        val future = CompletableFuture<Unit>()
+
+        // Run the save operation on the EDT
+        ApplicationManager.getApplication().invokeLater {
+            try {
+                // Save the currently unsaved changes
+                val fileDocumentManager = FileDocumentManager.getInstance()
+                fileDocumentManager.saveDocument(document)
+                future.complete(Unit) // Complete the future upon successful save
+            } catch (e: Exception) {
+                future.completeExceptionally(e) // Complete exceptionally in case of error
+            }
+        }
+
+        // Wait for the future (i.e. save operation) to complete
+        future.get()
+
+        // Now run the linter command
         val nioFile = file.virtualFile.toNioPath()
         return SqlFluffLintRunner.Param(
             execPath = python,
