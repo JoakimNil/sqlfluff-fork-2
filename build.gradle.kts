751122691b1eb41b2916c98e0bc3af150369f093
JoakimNil <24590972+JoakimNil@users.noreply.github.com>
2023-12-11 19:17:03 +0100

    update gradle ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.16.1"
 }
 
 group = "co.anbora.labs"
@@ -42,8 +42,8 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("222")
-        untilBuild.set("232.*")
+        sinceBuild.set("233")
+        untilBuild.set("233.*")
         changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
         pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
     }
